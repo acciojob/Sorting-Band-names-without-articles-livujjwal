7bd7e5803592d933e09aee1ba1fdd4ff551c9693
Ujjwal Singh <132872642+livujjwal@users.noreply.github.com>
2023-08-13 16:41:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-//your code here
-
+let touristSpots = ['The Virupaksha Temple', 'A Victoria Memorial', 'The Tajmahal', 'The Amarnath Temple','An Okhla Market'];
+let arr = [];
+let map = {};
+let result = [];
+for (let i of touristSpots) {
+	let tempArr = i.split(' ');
+	let tempStr = '';
+	for (let j of tempArr) {
+		tempWord = j.toLowerCase();
+		if(tempword !== 'the' && tempword !== 'an' && tempword !== 'a')
+		{
+			tempStr += j;
+		}
+	}
+	arr.push(tempStr);
+	map[tempStr] = i;
+}
+arr.sort();
+for (let i of arr) {
+result.push(map[i])
+}
+console.log(result);
